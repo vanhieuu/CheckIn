9da11a6c35564b410cc6e0a0eccfe125398a73b6
Vhieu <vanhieuu99@gmail.com>
2020-11-03 21:32:20 +0700

    update PopUp

--- Project/js/Calendar/PopUpAddEvent.js
+++ Project/js/Calendar/PopUpAddEvent.js
@@ -1,54 +1,70 @@
 let $template = document.getElementById("popup-template");
-    class AddEvent extends HTMLElement{
-       
-        constructor(){
-           console.log('e');
-            super()
-            this.attachShadow({ mode: 'open' });// Có cũng được mà không cũng được
-            this.shadowRoot.appendChild($template.content.cloneNode(true));
-            this.$modal = this.shadowRoot.getElementById('popup1')
-            this.$formAddEvent = this.shadowRoot.getElementById('add-event');
-            this.$timeIn = this.shadowRoot.getElementById('check-in-time');
-            this.$timeOut = this.shadowRoot.getElementById('check-out-time');
-            this.$getTime = this.shadowRoot.getElementById('btn-get-time');
-            
-            this.$formAddEvent.addEventListener('submit',(e)=>{
-                e.preventDefault();
-            
-                if(e.target.timeIn.value === "") return ;
-                if(e.target.timeOut.value === "") return ;
-                
-            })
-            this.$formAddEvent.onsubmit = (event) =>{    
-                    event.preventDefault();
-                    const timeIn = this.$timeIn.value;
-                    const timeOut =  this.$timeOut.value;
-                    const workTime = JSON.parse(localStorage.getItem('workTime')) || [];
-                    const timeWork = {
-                        timeIn: timeIn,
-                        timeOut: timeOut
-                    };
-
-                    workTime.push(timeWork);
-                    localStorage.setItem('timeWork',JSON.stringify(workTime));
-                    console.log(timeWork);
-            }
+class AddEvent extends HTMLElement {
+    index = 1;
+    constructor() {
+        super()
+        if (!localStorage.getItem('timeWork')) {
+            localStorage.setItem('timeWork', "[]")
         }
+        this.attachShadow({ mode: 'open' });// Có cũng được mà không cũng được
+        this.shadowRoot.appendChild($template.content.cloneNode(true));
+        this.$modal = this.shadowRoot.getElementById('popup1')
+        this.$formAddEvent = this.shadowRoot.getElementById('add-event');
+        this.$timeIn = this.shadowRoot.getElementById('check-in-time');
+        this.$timeOut = this.shadowRoot.getElementById('check-out-time');
+        this.$getTime = this.shadowRoot.getElementById('btn-get-time');
+        this.$list = this.shadowRoot.getElementById('list')
+      
+        this.$formAddEvent.addEventListener('submit', (e) => {
+            e.preventDefault();
 
-        static get observedAttributes(){
-            return ['show'];
-        }
-        attributeChangedCallback(name,old,newVal){
-            if(name == 'show'){
-            
-                if(newVal == 'false'){
-                    this.$modal.style.display = 'none';
-                }else{
-                    this.$modal.style.display = 'block';
-                }
-            }
-            console.log(name,old,newVal);
-         
+            if (e.target.timeIn.value === "") return;
+            if (e.target.timeOut.value === "") return;
+
+        })
+        this.$formAddEvent.onsubmit = (event) => {
+            event.preventDefault();
+            const timeIn = this.$timeIn.value;
+            const timeOut = this.$timeOut.value;
+            const workTime = JSON.parse(localStorage.getItem('workTime')) || [];
+            const timeWork = {
+                timeIn: timeIn,
+                timeOut: timeOut
+            };
+
+            workTime.push(timeWork);
+            localStorage.setItem('timeWork', JSON.stringify(workTime));
+            this.render();
         }
     }
-    window.customElements.define('show-pop-up',AddEvent)
+
+    static get observedAttributes() {
+        return ['show'];
+    }
+    attributeChangedCallback(name, old, newVal) {
+        if (name == 'show') {
+
+            if (newVal == 'false') {
+                this.$modal.style.display = 'none';
+            } else {
+                this.$modal.style.display = 'block';
+            }
+        }
+        console.log(name, old, newVal);
+
+    }
+    render() {
+        if (JSON.parse(localStorage.getItem('timeWork')).length > 0) {
+            let data = JSON.parse(localStorage.getItem('timeWork'));
+            
+            let htmlParse = data.sort((time1, time2) => time1 - time2).map((element) => {
+                return `<li>Ca ${this.index++} : ${element.timeIn}-${element.timeOut}</li>`
+            }).join("");
+            this.$list.appendChild(htmlParse);
+        }
+    }
+    convertMoment(time){
+        return moment(time)
+    }
+}
+window.customElements.define('show-pop-up', AddEvent)
--- Project/js/Calendar/calendar.js
+++ Project/js/Calendar/calendar.js
@@ -1,91 +1,87 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-    let today = new Date();
-    let currenMonth = today.getMonth();
-    let currentYears = today.getFullYear();
-    let modal = document.getElementById('popup');
-    console.log(modal);
-    let months = ["Jan"
-                ,"Feb"
-                ,"Mar"
-                ,"Apr"
-                ,"May"
-                ,"Jun"
-                ,"July"
-                ,"Aug"
-                ,"Sep"
-                ,"Oct"
-                ,"Nov"
-                ,"Dec"];
-    
-    let monthAndYear = document.getElementById('montAndYear');
-    
-        showCalendar( currenMonth ,currentYears)
-    
-    function showCalendar(month, year) {
-        let firstDay = new Date(year,month).getDay();
-        let daysInMonth = 32 - new Date(year,month,32).getDate();
-    
-        let tbl = document.getElementById('calendar-body');
-        tbl.innerHTML = "";
-        monthAndYear.innerHTML = months[month] + " " + year ;
-        
-        let date = 1;
-        for (let i = 0; i < 6; i++) {
-            let row = document.createElement('tr');
-            for (let j = 0; j < 7; j++) {
-                if (i === 0 && j < firstDay) {
-                    let cell = document.createElement("td");
-                    let cellText = document.createTextNode("");
-                    cell.appendChild(cellText);
-                    row.appendChild(cell);
-                    console.log(cellText);
+
+let today = new Date();
+let currenMonth = today.getMonth();
+let currentYears = today.getFullYear();
+let modal = document.getElementById('popup');
+let current = null;
+let months = ["Jan"
+    , "Feb"
+    , "Mar"
+    , "Apr"
+    , "May"
+    , "Jun"
+    , "July"
+    , "Aug"
+    , "Sep"
+    , "Oct"
+    , "Nov"
+    , "Dec"];
+
+let monthAndYear = document.getElementById('montAndYear');
+
+showCalendar(currenMonth, currentYears)
+
+function showCalendar(month, year) {
+    let firstDay = new Date(year, month).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById('calendar-body');
+    tbl.innerHTML = "";
+    monthAndYear.innerHTML = months[month] + " " + year;
+
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        let row = document.createElement('tr');
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                console.log(cellText);
+            }
+            else if (date > daysInMonth) {
+                break;
+            }
+            else {
+                let cell = document.createElement("td")
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
                 }
-                else if (date > daysInMonth) {
-                    break;
-                }
-                        else{
-                            let cell = document.createElement("td")
-                            let cellText = document.createTextNode(date);
-                            if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                                cell.classList.add("bg-info");
-                            }
-                            cell.appendChild(cellText);
-                            row.appendChild(cell);
-                              date++;
-                }
-              
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
         }
-    
         // console.log(row);
         [...row.querySelectorAll("tr td")].forEach(el => {
             el.onclick = (e) => {
+                if(current) {
+                   current.style.backgroundColor = "white";
+                }
+                // console.log(current);
+                current = e.target;
                 modal.setAttribute("show", "true")
+                e.target.style.backgroundColor = e.target.style.backgroundColor ==='yellow' ? 'white' : "yellow";
+
             }
         })
         tbl.appendChild(row)
     }
-    }
-        function Next(){
-        currentYears = (currenMonth === 11) ? currentYears + 1:currentYears;
-        currenMonth= (currenMonth + 1)%12;
-        showCalendar(currenMonth,currentYears);
-    }
-        function Previous() {
-        currentYears = (currenMonth === 0) ? currentYears -1 : currentYears;
-        currenMonth = (currenMonth === 0) ? 11 : currenMonth - 1
-        showCalendar(currenMonth,currentYears)
-    }
-        function showPopUp(){
-            onclick = () =>{
-                modal.classList.add("show");
-                
-            }
-        }
-    
 
+}
 
-
-
-})
+function Next() {
+    currentYears = (currenMonth === 11) ? currentYears + 1 : currentYears;
+    currenMonth = (currenMonth + 1) % 12;
+    showCalendar(currenMonth, currentYears);
+}
+function Previous() {
+    currentYears = (currenMonth === 0) ? currentYears - 1 : currentYears;
+    currenMonth = (currenMonth === 0) ? 11 : currenMonth - 1
+    showCalendar(currenMonth, currentYears)
+}
 
