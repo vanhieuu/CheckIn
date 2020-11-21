e7991936dc6cf2d73badcb78bad4d3a653a138b7
Vhieu <vanhieuu99@gmail.com>
2020-11-21 19:59:37 +0700

    Done

--- Project/js/Calendar/chuyentrang.js
+++ Project/js/Calendar/chuyentrang.js
@@ -1,4 +1,4 @@
 function chuyentrang() {
-    window.location.replace('./js/Calendar/salary.html')
+    window.location.replace('./Project/js/Calendar/salary.html')
     // /js/Calendar/salary.html
 }
